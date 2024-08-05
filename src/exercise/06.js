fa697d5c69e54d076fec65c2c31e877b2a15458e
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-05 15:06:11 +0430

    06 exercise extra credit 2:  Validate lower-case

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,38 +1,46 @@
-// Basic Forms
-// http://localhost:3000/isolated/exercise/06.js
+// Dynamic Forms
+// 💯 Validate lower-case
+// http://localhost:3000/isolated/final/06.extra-2.js
 
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  const ref = React.useRef(null)
+  const [error, setError] = React.useState(null)
 
   function handleSubmit(event) {
     event.preventDefault()
+    onSubmitUsername(event.target.elements.usernameInput.value)
+  }
 
-    // const value = event.target[0].value
-    // const value = event.target.elements.userNameInput.value
-
-    const value = ref.current.value
-
-    // console.dir(event.target.elements.userNameInput.value)
-
-    onSubmitUsername(value)
+  function handleChange(event) {
+    const {value} = event.target
+    const isLowerCase = value === value.toLowerCase()
+    setError(isLowerCase ? null : 'Username must be lower case')
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor="userNameInput">Username:</label>
-        <input ref={ref} id="userNameInput" type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input id="usernameInput" type="text" onChange={handleChange} />
       </div>
-      <button type="submit">Submit</button>
+      <div role="alert" style={{color: 'red'}}>
+        {error}
+      </div>
+      <button disabled={Boolean(error)} type="submit">
+        Submit
+      </button>
     </form>
   )
 }
 
 function App() {
   const onSubmitUsername = username => alert(`You entered: ${username}`)
-  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+  return (
+    <div style={{minWidth: 400}}>
+      <UsernameForm onSubmitUsername={onSubmitUsername} />
+    </div>
+  )
 }
 
 export default App
